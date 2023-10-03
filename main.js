e1831481241d342073d4685a637eb74c0a1b721c
DavidMuhicFRI <dm4695@student.uni-lj.si>
2023-10-03 22:23:42 +0200

    updated socket url

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket('ws://localhost:8080');
+const ws = new WebSocket('wss://projectaledav1.onrender.com/8080');
 
 ws.onopen = () => {
     console.log('WebSocket client connected');
