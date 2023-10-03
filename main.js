61967c05343e6a6d62400557d426def4f357ed68
DavidMuhicFRI <dm4695@student.uni-lj.si>
2023-10-04 00:32:06 +0200

    updated with pairs and clients

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-const ws = new WebSocket('wss://projectaledav1.onrender.com');
+window.onload = function() {
+    const ws = new WebSocket('wss://projectaledav1.onrender.com');
+    ws.onopen = () => {
+        console.log('WebSocket client connected');
+        ws.send('Hello, server!');
 
-ws.onopen = () => {
-    console.log('WebSocket client connected');
-    ws.send('Hello, server!');
-};
-
-ws.onmessage = (event) => {
-    const message = event.data;
-    console.log(`Received message from server: ${message}`);
-    // Handle incoming messages from the server
-};
-
-ws.onclose = () => {
-    console.log('closed');
-};
+    };
+    ws.onmessage = (message) => {
+        console.log(`Received message from server: ${message}`);
+        const dataReceived = JSON.parse(message);
+        console.log('Message:', dataReceived.message);
+        console.log('pairLeft:', dataReceived.left);
+        console.log('pairTop:', dataReceived.top);
+    };
+    ws.onclose = () => {
+        console.log('closed');
+    };
+    const playerBox1 = document.getElementById("playerBox");
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "a") {
+               playerBox1.style.left = playerBox1.offsetLeft - 3 + "px";
+        }else if (event.key === "w") {
+            playerBox1.style.top = playerBox1.offsetTop - 3 + "px";
+        }else if (event.key === "d") {
+            playerBox1.style.left = playerBox1.offsetLeft + 3 + "px";
+        }else if (event.key === "s") {
+            playerBox1.style.top = playerBox1.offsetTop + 3 + "px";
+        }
+    });
+}
--- server.js
+++ server.js
@@ -1,17 +1,65 @@
 const WebSocket = require('ws');
+let playerCount = 0;
+let pairCount = 0;
+let clients = [];
+let pairs = [];
 const wss = new WebSocket.Server({ port: 8080 });
-console.log('WebSocket server is starting...');
+console.log('WebSocket server is running!');
 wss.on('connection', (ws) => {
     console.log('connected');
+    ws.send(`Welcome, Client ${playerCount}!`);
+    let c;
+    if(playerCount % 2 === 0){
+        c = new Client(playerCount, ws, null, 200, 200, null);
+        playerCount++;
+        clients.push(c);
+        let p = new Pair(c, null);
+        pairs.push(p);
+        ws.send("searching for a pair...");
+    }else{
+        c = new Client(playerCount, ws, clients[playerCount - 1], 500, 200, null);
+        clients[playerCount - 1].pair = c;
+        playerCount++;
+        pairs[pairCount].p2 = c;
+        pairCount++;
+        ws.send("pair found: " + c.pair.id + "!");
+        c.pair.ws.send("pair found: " + c.id + "!");
+    }
     ws.on('message', (message) => {
         console.log(`Received message from client: ${message}`);
-        ws.send("sup homey");
-        // Handle incoming messages from clients
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                // Broadcast the message to all other clients
-                client.send("wassup idiot");
-            }
-        });
+        let decomposed = JSON.parse(message);
+        if(decomposed.reason === "name"){
+            clients.forEach(function(client){
+                if(ws === client.ws){
+                    client.name = decomposed.name;
+                }
+            })
+        }
+        sendData(ws, message);
     });
 });
+function sendData(socket, message){
+    clients.forEach(function(client){
+        if(client.ws === socket){
+            let pair = client.pair.ws;
+            message = JSON.stringify(client);
+            pair.send(message);
+        }
+    });
+}
+class Client{
+    constructor(id, ws, pair, left, top, name) {
+        this.id = id;
+        this.pair = pair;
+        this.ws = ws
+        this.left = left;
+        this.top = top;
+        this.name = name;
+    }
+}
+class Pair{
+    constructor(p1, p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+}
